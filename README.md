d77cdd4213ccd2bb20de0ea0247c9050245b70b5
Magnun A V F <magnunavf@gmail.com>
2021-06-19 21:54:42 -0300

    Add Angular Material with custom theme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularSsrMyPosts
+# Angular SSR My Posts
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.5.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
